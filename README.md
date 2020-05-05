b4fb8f14cdaf876c44b83f8719f751e1572c19d2
MiGueAJM9724 <miguejm@outlook.es>
2020-05-04 22:03:08 -0500

    FirtsCommit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WebServiceApplicationSwift
---
+++ WebServiceProduct/TableViewCellProduct.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellProduct.swift
+//  WebServiceProduct
+//
+//  Created by Jimenez Melendez Miguel Angel on 04/05/20.
+//  Copyright © 2020 Jimenez Melendez Miguel Angel. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellProduct: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
