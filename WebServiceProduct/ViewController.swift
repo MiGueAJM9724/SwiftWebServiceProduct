17ff03fa284133897558a55db27039fe2d4f370f
MiGueAJM9724 <miguejm@outlook.es>
2020-05-04 21:28:16 -0500

    Initial Commit

---
+++ WebServiceProduct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServiceProduct
+//
+//  Created by Jimenez Melendez Miguel Angel on 04/05/20.
+//  Copyright © 2020 Jimenez Melendez Miguel Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WebServiceProductTests/WebServiceProductTests.swift
@@ -0,0 +1,34 @@
+//
+//  WebServiceProductTests.swift
+//  WebServiceProductTests
+//
+//  Created by Jimenez Melendez Miguel Angel on 04/05/20.
+//  Copyright © 2020 Jimenez Melendez Miguel Angel. All rights reserved.
+//
+
+import XCTest
+@testable import WebServiceProduct
+
+class WebServiceProductTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
